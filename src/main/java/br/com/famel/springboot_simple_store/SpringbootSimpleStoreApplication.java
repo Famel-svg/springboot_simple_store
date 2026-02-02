805a03b208c98520467a52632dfad6ddb8f654a1
Rafael <rafaelandradetanimoto@gmail.com>
2026-02-02 20:57:48 -0300

    Project Created

---
+++ src/main/java/br/com/famel/springboot_simple_store/SpringbootSimpleStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.famel.springboot_simple_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSimpleStoreApplication.class, args);
+    }
+
+}
