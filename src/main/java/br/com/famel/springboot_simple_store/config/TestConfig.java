0f18e1e907832444a4075e1b3056eab6457bdb56
Rafael <rafaelandradetanimoto@gmail.com>
2026-02-11 19:21:01 -0300

    OrderStatus Enum

--- src/main/java/br/com/famel/springboot_simple_store/config/TestConfig.java
+++ src/main/java/br/com/famel/springboot_simple_store/config/TestConfig.java
@@ -1,6 +1,7 @@
 package br.com.famel.springboot_simple_store.config;
 
 import br.com.famel.springboot_simple_store.entities.Order;
+import br.com.famel.springboot_simple_store.entities.OrderStatus;
 import br.com.famel.springboot_simple_store.entities.User;
 import br.com.famel.springboot_simple_store.repositories.OrderRepository;
 import br.com.famel.springboot_simple_store.repositories.UserRepository;
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null,"Maria","Maria@gmail.com","98888888","123456");
         User u2 = new User(null,"Alex","Alex@gmail.com","9777777","123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT , u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/br/com/famel/springboot_simple_store/entities/Order.java
+++ src/main/java/br/com/famel/springboot_simple_store/entities/Order.java
@@ -19,6 +19,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -33,9 +35,10 @@ public class Order implements Serializable {
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -51,6 +54,15 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 
     @Override
     public String toString() {
